845567350c605f88b3aeca9518934a72874e59b0
GWTN156-4\sima <www.simabilony.com>
2023-10-04 03:00:32 -0700

    commit init

---
+++ app/Models/sections.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class sections extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'section_name',
+        'description',
+        'Created_by',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\InvoicesController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-
+use App\Http\Controllers\Customers_Report;
+use App\Http\Controllers\InvoiceAttachmentsController;
+use App\Http\Controllers\InvoicesDetailsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoicesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,16 +18,59 @@ use App\Http\Controllers\AdminController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-//Route::get('/{page}', 'AdminController@index'); //laravel8
+Route::view('/' , 'auth.login');
+
 Auth::routes();
+
+Route::resource('/invoices' , InvoicesController::class);
+
+Route::resource('/sections' , SectionController::class);
+
+Route::resource('/products' , ProductController::class);
+
+Route::resource('/InvocieAttachments' , InvoiceAttachmentsController::class);
+
+Route::group(['middleware' => ['auth']], function() {
+
+    Route::resource('/roles', RoleController::class);
+
+    Route::resource('/users', UserController::class);
+
+});
+
+Route::get('/InvoicesDetails/{id}' , [InvoicesDetailsController::class , "edit"]);
+
+Route::get('/download/{invoice_number}/{file_name}' , [InvoicesDetailsController::class , "open_file"]);
+
+Route::get('/delete_file' , [InvoicesDetailsController::class , "destroy"])->name('delete_file');
+
+Route::get('/InvoicesDetails/{id}' , [InvoicesDetailsController::class , "edit"]);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('invoices',InvoicesController::class);
-Route::get('/{page}', [AdminController::class, 'index']);
+
+///******//////
+Route::get('Status_show/{id}' , [InvoicesController::class , 'show'])->name('Status_show');
+
+Route::post('Status_Update/{id}' , [InvoicesController::class , 'Status_Update'])->name('Status_Update');
+
+Route::get('/Invoice_Paid' , [InvoicesController::class , 'invoice_Paid']);
+
+Route::get('/Invoice_UnPaid' , [InvoicesController::class , 'invoice_UnPaid']);
+
+Route::get('/Invoice_Partial' , [InvoicesController::class , 'invoice_Partial']);
+
+Route::get('/Print_invoice/{id}' , [InvoicesController::class , 'print_index']);
+
+Route::get('/sections/{id}' , [InvoicesController::class , "getproducts"]);
+
+/////*****//////
+Route::get('/customer_report' , [Customers_Report::class , 'index'])->name('customer_report');
+
+Route::get('/Search_customer' , [Customers_Report::class , 'Search_customers']);
+
+Route::get('/{page}', [AdminController::class , "index"]);
